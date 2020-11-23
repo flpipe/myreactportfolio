88ddd8ad9d3edf0128406c6be92d8739a39ada32
Revathy Venkataraman <revathyvenka@gmail.com>
2020-11-23 09:07:32 -0500

    Basic Portfolio template work upload

---
+++ src/components/landingpage.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react';
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div><h1>LandingPage - Work in Progress </h1></div>
+    )
+  }
+}
+
+export default LandingPage;
